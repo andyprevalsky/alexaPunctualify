51bc31368496433799935815ced5c726f6628694
Andy Prevalsky <andyprevalsky@yahoo.com>
2018-11-04 08:31:15 -0600

    further testing

--- index.js
+++ index.js
@@ -20,9 +20,9 @@ const languageStrings = {
             FACTS: [
                 'You have an meet up soon, leave in 5 minutes!',
             ],
-            SKILL_NAME: 'British Space Facts',
+            SKILL_NAME: 'meet up',
             GET_FACT_MESSAGE: "Alert! ",
-            HELP_MESSAGE: 'You can say tell me a space fact, or, you can say exit... What can I help you with?',
+            HELP_MESSAGE: 'meet up',
             HELP_REPROMPT: 'What can I help you with?',
             STOP_MESSAGE: 'Goodbye!',
         },
@@ -32,9 +32,9 @@ const languageStrings = {
             FACTS: [
                 'The Moon is moving approximately 3.8 cm away from our planet every year.',
             ],
-            SKILL_NAME: 'American Space Facts',
-            GET_FACT_MESSAGE: "Here's your fact: ",
-            HELP_MESSAGE: 'You can say tell me a space fact, or, you can say exit... What can I help you with?',
+            SKILL_NAME: 'meet up',
+            GET_FACT_MESSAGE: "Alert! ",
+            HELP_MESSAGE: 'meet up',
             HELP_REPROMPT: 'What can I help you with?',
             STOP_MESSAGE: 'Goodbye!',
         },
@@ -42,19 +42,7 @@ const languageStrings = {
     'de-DE': {
         translation: {
             FACTS: [
-                'Ein Jahr dauert auf dem Merkur nur 88 Tage.',
-                'Die Venus ist zwar weiter von der Sonne entfernt, hat aber höhere Temperaturen als Merkur.',
-                'Venus dreht sich entgegen dem Uhrzeigersinn, möglicherweise aufgrund eines früheren Zusammenstoßes mit einem Asteroiden.',
-                'Auf dem Mars erscheint die Sonne nur halb so groß wie auf der Erde.',
-                'Die Erde ist der einzige Planet, der nicht nach einem Gott benannt ist.',
-                'Jupiter hat den kürzesten Tag aller Planeten.',
-                'Die Milchstraßengalaxis wird in etwa 5 Milliarden Jahren mit der Andromeda-Galaxis zusammenstoßen.',
-                'Die Sonne macht rund 99,86 % der Masse im Sonnensystem aus.',
-                'Die Sonne ist eine fast perfekte Kugel.',
-                'Eine Sonnenfinsternis kann alle ein bis zwei Jahre eintreten. Sie ist daher ein seltenes Ereignis.',
-                'Der Saturn strahlt zweieinhalb mal mehr Energie in den Weltraum aus als er von der Sonne erhält.',
-                'Die Temperatur in der Sonne kann 15 Millionen Grad Celsius erreichen.',
-                'Der Mond entfernt sich von unserem Planeten etwa 3,8 cm pro Jahr.',
+                'You have a meetup soon! Leave in 5 minutes to get there on time!'
             ],
             SKILL_NAME: 'Weltraumwissen auf Deutsch',
             GET_FACT_MESSAGE: 'Hier sind deine Fakten: ',
